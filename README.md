e9f8afb4fd14f8c84a0f2337ac93fd88f1996157
Nick <nickpatricklawrence@gmail.com>
2025-01-05 01:46:15 -0500

    cleaner logic, logging

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Video transcriber
-Video transcriber that utilizes OpenAI Whisper & yt-dlp
+Video transcriber that utilizes OpenAI Whisper & yt-dlp to download and transcribe a video's audio using your local machine
--- transcribe.py
+++ transcribe.py
@@ -2,24 +2,28 @@ import yt_dlp
 import whisper
 import sys
 import os
+from whisper.utils import get_writer
+import logging
 
 def transcribe(link):
-    downloaded_filename = downloader.prepare_filename(downloader.extract_info(link, download=False))
+    try:
+        downloaded_filename = downloader.prepare_filename(downloader.extract_info(link, download=False))
+        dirname = downloaded_filename.split('.')[0]
+        os.makedirs(dirname)
+    except:
+        logging.warning(f'Output directory already exists, skipping {link}')
+        return
     downloader.download(link)
     result = model.transcribe(downloaded_filename)
-    # Save the transcription to a file in your desired directory
-    dirname = downloaded_filename.split('.')[0]
-    os.makedirs(dirname)
-    with open(os.path.join(dirname, "transcription.txt"), "w") as f:
-        f.write(result['text'])
+    # Write to output directory
+    get_writer('txt', dirname)(result, 'transcript.txt')
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print('Correct usage: python3 transcribe.py [video_link1], [video_link2], ...')
     else:
-        processes = []
         model = whisper.load_model("tiny.en")
-        downloader = yt_dlp.YoutubeDL(params={'format':'ba', 'outtmpl' : {'default': f"%(upload_date)s.mp3"}})
+        downloader = yt_dlp.YoutubeDL(params={'format':'ba', 'outtmpl' : {'default': f"%(title)s_%(upload_date)s.mp3"}})
         for x in sys.argv[1::]:
            transcribe(x)
 
